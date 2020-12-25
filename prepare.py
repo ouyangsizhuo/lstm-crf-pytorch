98b0cd7dbf6b92ed3650844314fbf822b42cd377
ouyangsizhuo <75346881+ouyangsizhuo@users.noreply.github.com>
2020-12-25 15:45:59 +0800

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -3,14 +3,14 @@ from utils import *
 def load_data():
     data = []
     if KEEP_IDX:
-        cti = load_tkn_to_idx(sys.argv[1] + ".char_to_idx")
-        wti = load_tkn_to_idx(sys.argv[1] + ".word_to_idx")
-        tti = load_tkn_to_idx(sys.argv[1] + ".tag_to_idx")
+        cti = load_tkn_to_idx("./prepare_data/train.txt" + ".char_to_idx")
+        wti = load_tkn_to_idx("./prepare_data/train.txt" + ".word_to_idx")
+        tti = load_tkn_to_idx("./prepare_data/train.txt" + ".tag_to_idx")
     else:
         cti = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
         wti = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX, UNK: UNK_IDX}
         tti = {PAD: PAD_IDX, SOS: SOS_IDX, EOS: EOS_IDX}
-    fo = open(sys.argv[1])
+    fo = open("./prepare_data/train.txt")
     if HRE:
         tmp = []
         txt = fo.read().strip().split("\n\n")
@@ -57,11 +57,9 @@ def load_line(line, cti, wti, tti):
     return x, y
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit("Usage: %s training_data" % sys.argv[0])
     data, cti, wti, tti = load_data()
-    save_data(sys.argv[1] + ".csv", data)
+    save_data("./prepare_data/train.txt" + ".csv", data)
     if not KEEP_IDX:
-        save_tkn_to_idx(sys.argv[1] + ".char_to_idx", cti)
-        save_tkn_to_idx(sys.argv[1] + ".word_to_idx", wti)
-        save_tkn_to_idx(sys.argv[1] + ".tag_to_idx", tti)
+        save_tkn_to_idx("./prepare_data/train.txt" + ".char_to_idx", cti)
+        save_tkn_to_idx("./prepare_data/train.txt" + ".word_to_idx", wti)
+        save_tkn_to_idx("./prepare_data/train.txt" + ".tag_to_idx", tti)
